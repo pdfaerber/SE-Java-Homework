978b63a335f8c3e5929ac29bbe6ee63500f3d951
pdfaerber <87340230+pdfaerber@users.noreply.github.com>
2021-10-05 18:21:14 -0400

    This is the Mod 3, Week 1, Homework 3: Calculator

--- CalculatorHW.java
+++ CalculatorHW.java
@@ -30,19 +30,100 @@
 	4. Return the answer and display it inside the main method.
 
 	5. Display a default message (ex. "Invalid Entry, Try Again") 
-   if any number other than 1-5 is entered by the user
+   	if any number other than 1-5 is entered by the user
 *******************************************************************************************/
 
 
 package calculatorHW;
 
+import java.util.Scanner;
+
 public class CalculatorHW {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Calculation Menu Number: \n1. Addition \n2. Subtraction \n3. Multiply \n4. Division \n5. Exit");
+		int menu = scan.nextInt();
+//		System.out.println(menu);
+		
+		System.out.println("Enter 2 Integers for Calculation: ");
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		
+		
+		
+		calculate(menu, num1, num2);
+		
+		
 	}//main
 	
+public static void calculate(int menu, int num1, int num2) {
+	System.out.println("menu: " + menu);
+	System.out.println(num1);
+	System.out.println(num2);
 	
+//	boolean test = false;	
+	String userMsg; 
+//	while(test) {
+		switch (menu) {
+		case 1:
+			System.out.println("The sum of your two numbers is: " + addition(num1, num2));
+			break;
+		case 2:
+			
+			System.out.println("The difference of your two numbers is: " + subtraction(num1, num2));
+			break;
+			
+		case 3:
+			System.out.println("The product of your two numbers is: " + multiply(num1, num2));
+			break;
+		
+		case 4:
+			System.out.println("The quotient of your two numbers is: " + divide(num1, num2));
+			break;
+		
+		case 5:
+			userMsg = "Are you sure you want to exit the program? ";
+			return;
+		
+		default:
+			userMsg = "Invalid Input";
+			break;
+		
+		}//end switch
+//		System.out.println(userMsg);
+//	}//end while(test)
+	 
+	
+}
+	public static int addition(int num1, int num2) {
+
+		int sum = num1 + num2;
+		return sum;
+		
+	}//addition
+	
+	public static int subtraction(int num1, int num2) {
+
+		int diff = num1 - num2;
+		return diff;
+		
+	}//endsubtraction
+	
+	public static int multiply(int num1, int num2) {
+
+		int prod = num1 * num2;
+		return prod;
+		
+	}//end multiply
+	
+	public static int divide(int num1, int num2) {
+
+		int quotient = num1 / num2;
+		return quotient;
+		
+	}//end multiply
+
 
 }//end CalculatorHW 
