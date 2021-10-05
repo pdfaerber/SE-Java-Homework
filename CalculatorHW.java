7a25cea4f47583699ca7b6aedd4147787db01865
pdfaerber <87340230+pdfaerber@users.noreply.github.com>
2021-10-05 09:25:56 -0400

    Mod 3, Week 1, Homework 3 java calculator

---
+++ CalculatorHW.java
@@ -0,0 +1,48 @@
+/*************************************************************************************
+	
+	Patrick D. Faerber, Per Scholas Software Engineering Cohort 13, October 5, 2021
+	
+	Module 3: Week 1, Homework 3
+	
+ *************************************************************************************	
+	Build a calculator:
+
+	Write a program to create a calculator that can Add, Subtract, Multiply, 
+	and Divide 2 numbers entered by the user. Also, add an option to exit out from the program.
+  	OPTIONS
+
+	1. Add
+
+	2. Subtract
+
+	3. Multiply
+
+	4. Divide
+
+	5. Exit
+
+	1. Write different methods for each module (add, subtract, multiply, and divide).
+
+	2. Use switch/case to call the particular method.
+
+	3. Take input inside the methods to perform the related operation.
+
+	4. Return the answer and display it inside the main method.
+
+	5. Display a default message (ex. "Invalid Entry, Try Again") 
+   if any number other than 1-5 is entered by the user
+*******************************************************************************************/
+
+
+package calculatorHW;
+
+public class CalculatorHW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}//main
+	
+	
+
+}//end CalculatorHW 
